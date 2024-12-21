5d0f1ce4231781fa03eca74c2953c2038f5cf7a0
Toanf2103 <100524773+Toanf2103@users.noreply.github.com>
2024-12-21 11:59:09 +0700

    Update environment.ts

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://pokemon-be-production-9ffb.up.railway.app/api',
+  apiUrl: 'https://pokemon-be-production-9ffb.up.railway.app',
 };
